4fde8bd47e962a7ad5302a1af43c217bc6693d76
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-04-20 12:07:15 +0300

    signin functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
-import {PrismaClient} from "@prisma/client"
+import {PrismaClient} from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -22,3 +23,28 @@ export const signup = async (req,res,next)=>{
         res.status(500).json({ error: "Failed to create user", because:error });
     }
 }
+
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        });
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+        
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+        
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        
+        res.status(200).json({ message: 'Sign in successful', token: token });
+    } catch (error) {
+        console.error('Error signing in:', error);
+        res.status(500).json({ error: 'Failed to sign in', because: error });
+    }
+};
