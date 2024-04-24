560ee74f7d3149081bedf086c7da7deced571f23
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-04-24 15:19:59 +0300

    authorizing the route

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { createCategory } from "../controllers/category.controller.js";
-
+import { authenticate } from "../middleware/authenticate.js";
+import { authorize } from "../middleware/authorize.js";
 const router = express.Router(); 
-router.post("/", createCategory);
+
+router.post("/", authenticate, authorize("ADMIN"),createCategory);
 
 export default router;
