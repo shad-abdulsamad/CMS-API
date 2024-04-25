c584967c651cbd9ac184d0d74da9c3edd1b15a74
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-04-25 14:41:00 +0300

    removed unnecessary functions

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,37 +51,6 @@ export const signup = async (req,res,next)=>{
 
 
 
-/* const jwtSecretKey = "jwtsecretkey"; // Your JWT secret key, replace with your actual secret key
-
-export const signin = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email: email },
-    });
-    if (!user) {
-      return res.status(401).json({ error: "User not found" });
-    }
-
-    const passwordMatch = bcrypt.compareSync(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: "Wrong Credentials" });
-    }
-
-    const token = jwt.sign({ userId: user.id }, jwtSecretKey);
-
-    // Set the token as a cookie
-    res.cookie("token", token, { httpOnly: true }); // You can set other cookie options as needed
-
-    res.status(200).json({ message: "Sign in successful" });
-  } catch (error) {
-    console.error("Error signing in:", error);
-    res.status(500).json({ error: "Failed to sign in", because: error });
-  }
-};
- */
-
-
 export const logout = (req,res)=>{
     try {
         res.clearCookie('token');
