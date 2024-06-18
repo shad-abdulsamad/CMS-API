5a28543690db5ea07bb3f5ceca1b4273090a3439
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-06-18 15:54:21 +0300

    get single post

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -1,12 +1,9 @@
-import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const secret = "jwtsecretkey";
 
-
-
-export const getSinglePost = async (req,res) =>{
+ export const getSinglePost = async (req,res) =>{
   const {id} = req.params;
     try {
       const post = await prisma.content.findUnique({
@@ -26,63 +23,10 @@ export const getSinglePost = async (req,res) =>{
       
     }
 }
+ 
 
 
-export const updateContent = async (req, res) => {
-  const { id } = req.params;
-  const { title, body, tags, categoryName } = req.body;
-  const token = req.headers.authorization;
 
-  try {
-    
-    const decoded = jwt.verify(token, secret);
-    const userId = decoded.userId;
-
-    const content = await prisma.content.update({
-      where: {
-        id: parseInt(id), 
-      },
-      data: {
-        title,
-        body,
-      },
-    });
-
-    if (Array.isArray(tags) && tags.length > 0) {
-      const tagIds = await Promise.all(
-        tags.map(async (tagName) => {
-          const tag = await prisma.tag.create({
-            data: { tag_name: tagName },
-          });
-          return tag.id;
-        })
-      );
-      await prisma.contentTag.createMany({
-        data: tagIds.map((tagId) => ({
-          content_id: content.id,
-          tag_id: tagId,
-        })),
-      });
-    }
-
-    if (categoryName) {
-      const category = await prisma.category.create({
-        data: { category_name: categoryName },
-      });
-      await prisma.categoryContent.create({
-        data: {
-          content_id: content.id,
-          category_id: category.id,
-        },
-      });
-    }
-
-    return res.status(200).json({ message: "Content updated successfully", content });
-  } catch (error) {
-    console.error("Error updating content:", error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-};
 export const deleteContent = async (req, res) => {
   const { id } = req.params;
 
@@ -194,4 +138,9 @@ export const getPosts = async (req, res) => {
         console.error('Error fetching posts:', error);
         res.status(500).json({ error: error.message });
     }
+};
+
+
+export const updateContent = async (req, res) => {
+    
 };
--- routes/content.route.js
+++ routes/content.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/", createContentByAdmin);
 router.get("/",getPosts);
-router.get("/:id",  getSinglePost);
+router.get("/:id", getSinglePost);
 router.put("/:id",  updateContent);
 router.delete("/:id", deleteContent);
 
